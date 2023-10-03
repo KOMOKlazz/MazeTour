6b8b5b34899b5ee60ac63fbe80afd0a0594fdcbd
Tikhon Dobrusov <komo4ek.yt@gmail.com>
2023-10-04 00:42:52 +0300

    CandyWar: fireball fix, doublejump, sounds, candies, other

--- src/main/java/komok/org/example/mazetour/MazeTour.java
+++ src/main/java/komok/org/example/mazetour/MazeTour.java
@@ -2,19 +2,23 @@ package komok.org.example.mazetour;
 
 import komok.org.example.mazetour.commands.*;
 import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Fireball;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -44,25 +48,51 @@ public final class MazeTour extends JavaPlugin implements Listener {
         System.out.println("MazeTour был остановлен!");
     }
 
-    @EventHandler(priority= EventPriority.HIGH)
+    //CANDY WAR →
+
+    @EventHandler
+    public void onPlayerDoubleJump(PlayerToggleFlightEvent event) {
+        Player player = (Player) event.getPlayer();
+        Bukkit.broadcastMessage("1");
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            event.setCancelled(true);
+            Block block = player.getWorld().getBlockAt(player.getLocation().subtract(0, 2, 0));
+            Bukkit.broadcastMessage("2");
+            if (!block.getType().equals(Material.AIR)) {
+                Vector vector = player.getLocation().getDirection().multiply(1).setY(1);
+                player.setVelocity(vector);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerUse(PlayerInteractEvent event){
         if (!candyWarStartCommand.isRun()) {return;}
-        Player p = event.getPlayer();
+        Player player = event.getPlayer();
 
-        if(event.getAction().equals(Action.RIGHT_CLICK_AIR)){
-            if(p.getItemInHand().getType() == Material.FEATHER){
-//                p.setVelocity();
+        if(event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+            if(player.getItemInHand().getType() == Material.FEATHER){
+                Bukkit.broadcastMessage("1");
+                if (player.getGameMode() != GameMode.CREATIVE) {
+                    event.setCancelled(true);
+                    Block block = player.getWorld().getBlockAt(player.getLocation().subtract(0, 2, 0));
+                    Bukkit.broadcastMessage("2");
+                    if (!block.getType().equals(Material.AIR)) {
+                        Vector vector = player.getLocation().getDirection().multiply(1.5).setY(1);
+                        player.setVelocity(vector);
+                    }
+                }
             }
-            else if(p.getItemInHand().getType() == Material.BRUSH){
-                Fireball fire = p.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class);
+            else if(player.getItemInHand().getType() == Material.BRUSH){
+                Fireball fire = player.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class);
                 fire.setFireTicks(0);
-                fire.setShooter(p);
+                fire.setShooter(player);
                 fire.setSilent(true);
 
-                Entity pig = p.getWorld().spawnEntity(p.getLocation(), EntityType.PIG);
-                for (Player player: Bukkit.getOnlinePlayers()) {
-                    player.hideEntity(getInstance(), fire);
-                    player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_LAUNCH, 1, 1);
+                Entity pig = player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
+                for (Player player_: Bukkit.getOnlinePlayers()) {
+                    player_.hideEntity(getInstance(), fire);
+                    player_.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_LAUNCH, 1, 1);
                 }
                 taskId = Bukkit.getScheduler().runTaskTimer(MazeTour.getInstance(), new Runnable() {
                     @Override
@@ -74,46 +104,95 @@ public final class MazeTour extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler (priority = EventPriority.HIGH)
+    public void onInventoryClick(InventoryClickEvent event) {
+//        if (event.getCurrentItem().getType() == )
+        event.setCancelled(true);
+    }
+
     @EventHandler
     public void entityDeathEvent(EntityDeathEvent event) {
+        Bukkit.broadcastMessage(ChatColor.YELLOW + "ENTITY DEATH");
         event.setDroppedExp(0);
-        for (ItemStack itemStack: event.getDrops()) {
+        for (Object itemStack: event.getDrops().toArray()) {
             event.getDrops().remove(itemStack);
         }
         Entity entity = event.getEntity();
+        if(entity.getLastDamageCause().getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION)) {
+            if (entity instanceof Player) {
+                Player taker = event.getEntity().getKiller();
+                Player shooter = event.getEntity().getKiller();
+                onPlayerExploded(taker, shooter);
+                return;
+            }
+        }
 //        if (entity.getHeight() - e.getDamage() < 0) {
 //            p.teleport(new Location(e.getEntity().getWorld(), 0.0, -400.0, 0.0));
 //        }
+        return;
     }
 
     @EventHandler
     public void onArrowHit(ProjectileHitEvent event) {
+        Bukkit.broadcastMessage(ChatColor.YELLOW + "FIREBALL HIT");
         Fireball fire = (Fireball) event.getEntity();
+        LivingEntity entity = (LivingEntity) event.getHitEntity();
         Player taker = (Player) event.getHitEntity();
         Player shooter = (Player) fire.getShooter();
+        if (entity != null) {entity.setHealth(0);}
         if (taker == null) {return;}
-        shooter.sendMessage( ChatColor.GRAY + "Вы убили " + ChatColor.RED + taker.getName());
-        taker.setHealth(0);
-        taker.sendTitle(ChatColor.RED + "Вас убили", "");
-        taker.sendMessage( ChatColor.GRAY + "Вы были убиты " + ChatColor.RED + shooter.getName());
-        taker.playSound(taker.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1, 1);
-        Inventory takerInventory = taker.getInventory();
-        for (ItemStack item: takerInventory.getContents()) {
-            Bukkit.broadcastMessage(item.getTranslationKey());
+        onPlayerExploded(taker, shooter);
+    }
+
+    public void onPlayerExploded(Player taker, Player shooter) {
+        if (shooter == taker) {
+            taker.sendTitle(ChatColor.RED + "СМЕРТЬ", "");
+            taker.sendMessage( ChatColor.RED + "Вы убили самого себя!");
+            taker.playSound(taker.getLocation(), Sound.ITEM_SHIELD_BLOCK, 1, 1);
+            return;
         }
+        shooter.sendMessage( ChatColor.RED + "Вы убили " + ChatColor.RED + taker.getName());
+        shooter.playSound(taker.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1, 1);
+        taker.sendTitle(ChatColor.RED + "СМЕРТЬ", "");
+        taker.sendMessage( ChatColor.RED + "Вы были убиты " + ChatColor.RED + shooter.getName());
+        taker.playSound(taker.getLocation(), Sound.ITEM_SHIELD_BLOCK, 1, 1);
+        changeCandies(shooter.getInventory(), 1);
+        changeCandies(taker.getInventory(), -1);
+    }
+
+    public void changeCandies(Inventory inventory, int amount) {
+        ItemStack candies = inventory.getItem(4);
+        candies.setAmount(candies.getAmount() + amount);
+        inventory.setItem(4, candies);
     }
 
     @EventHandler
     public void onPlayerMessage(PlayerChatEvent event) {
-        Player player = (Player) event.getPlayer();
-        World world = (World) Bukkit.getServer().getWorld("world");
-        Location chestLocation = new Location(world, -100, 300, 0);
-        Chest chest = (Chest) chestLocation.getBlock().getState();
-        ItemStack brush = chest.getInventory().getItem(1);
-        ItemStack candies = chest.getInventory().getItem(4);
-        Inventory takerInventory = player.getInventory();
-        takerInventory.setItem(1, brush);
-        takerInventory.setItem(4, candies);
-        event.setFormat("%s %s");
+        event.setFormat("%s: %s");
+    }
+
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent event){
+        if(event.getItemDrop().getItemStack().getType() == Material.BRUSH ||
+                event.getItemDrop().getItemStack().getType() == Material.PLAYER_HEAD) {
+            event.setCancelled(true);
+        }
+    }
+
+    //CANDY WAR ←
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e)
+    {
+        e.setDeathMessage(null);
+    }
+
+    @EventHandler
+    public void onFall(EntityDamageEvent event) {
+        if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+            if(event.getEntityType()==EntityType.PLAYER) {
+                event.setCancelled(true);
+            }
+        }
     }
 }
--- src/main/java/komok/org/example/mazetour/commands/candyWarStartCommand.java
+++ src/main/java/komok/org/example/mazetour/commands/candyWarStartCommand.java
@@ -2,6 +2,7 @@ package komok.org.example.mazetour.commands;
 
 import komok.org.example.mazetour.MazeTour;
 import org.bukkit.*;
+import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,6 +13,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
@@ -26,6 +29,7 @@ public class candyWarStartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        World world = (World) Bukkit.getServer().getWorld("world");
         Bukkit.broadcastMessage(ChatColor.RED + "Конфетные войны " + ChatColor.YELLOW + "через 30 секунд!");
         taskId = Bukkit.getScheduler().runTaskTimer(plugin, new Runnable() {
             int time = 0;
@@ -36,32 +40,44 @@ public class candyWarStartCommand implements CommandExecutor {
 //                        Bukkit.broadcastMessage(ChatColor.RED + "Конфетные войны " + ChatColor.YELLOW + "через 10 секунд!");
 //                        break;
                     case 0: //30
-                        World world = (World) Bukkit.getServer().getWorld("world");
                         Location location =  new Location(world, 3000, 65, 0);
                         for (Player player: Bukkit.getOnlinePlayers()) {
                             player.teleport(location);
+                            player.setGameMode(GameMode.ADVENTURE);
+                            player.setHealth(20);
                         }
 //                        cancelTask(taskId);
                         break;
                     case 17:
                         for (Player player: Bukkit.getOnlinePlayers()) {
                             player.sendTitle(ChatColor.YELLOW + "3", "");
+                            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
                         }
                         break;
                     case 18:
                         for (Player player: Bukkit.getOnlinePlayers()) {
                             player.sendTitle(ChatColor.YELLOW + "2", "");
+                            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
                         }
                         break;
                     case 19:
                         for (Player player: Bukkit.getOnlinePlayers()) {
                             player.sendTitle(ChatColor.YELLOW + "1", "");
+                            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
                         }
                         break;
                     case 20:
                         for (Player player: Bukkit.getOnlinePlayers()) {
+                            Location chestLocation = new Location(world, -100, 300, 0);
+                            Chest chest = (Chest) chestLocation.getBlock().getState();
+                            ItemStack brush = chest.getInventory().getItem(0);
+                            ItemStack candies = chest.getInventory().getItem(4);
+                            Inventory takerInventory = player.getInventory();
+                            takerInventory.setItem(0, brush);
+                            takerInventory.setItem(4, candies);
                             player.sendTitle(ChatColor.RED + "Старт", "");
                             player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+                            player.playSound(player.getLocation(), Sound.ENTITY_WITCH_CELEBRATE, 1, 1);
                             run = true;
                         }
                         break;
