0a4a6c4dde6d6f8c7a0dfb645eb366e9429ca337
Groot0700 <ejik228666@gmail.com>
2023-10-06 19:12:25 +0300

    teleport

--- src/main/java/komok/org/example/mazetour/commands/boatRaceCommand.java
+++ src/main/java/komok/org/example/mazetour/commands/boatRaceCommand.java
@@ -22,72 +22,72 @@ public class boatRaceCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-//        if (args.length == 0) {
-//            sender.sendMessage(ChatColor.RED + "Выбери действие!");
-//            return false;
-//        }
-//        switch (args[0]) {
-//            case "start":
-//                if (run) {
-//                    sender.sendMessage(ChatColor.RED + "Испытание уже начато!");
-//                    break;
-//                }
-//                run = true;
-//                World world = (World) Bukkit.getServer().getWorld("world");
-//                Bukkit.broadcastMessage(ChatColor.YELLOW + "Другое испытание начнется через 30 секунд!");
-//                taskId = Bukkit.getScheduler().runTaskTimer(plugin, new Runnable() {
-//                    int time = 0;
-//
-//                    @Override
-//                    public void run() {
-//                        switch (time) {
-//                            //                    case 20:
-//                            //                        Bukkit.broadcastMessage(ChatColor.YELLOW + "Перемещение на другое испытание через 10 секунд!");
-//                            //                        break;
-//                            case 0:
-//                                World world = (World) Bukkit.getServer().getWorld("world");
-//                                Location location = new Location(world, 1500, 63, 0);
-//                                for (Player player : Bukkit.getOnlinePlayers()) {
-//                                    player.teleport(location);
-//                                }
-//                                break;
-//                            case 17:
-//                                for (Player player : Bukkit.getOnlinePlayers()) {
-//                                    player.sendTitle(ChatColor.YELLOW + "3", "");
-//                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
-//                                }
-//                                break;
-//                            case 18:
-//                                for (Player player : Bukkit.getOnlinePlayers()) {
-//                                    player.sendTitle(ChatColor.YELLOW + "2", "");
-//                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
-//                                }
-//                                break;
-//                            case 19:
-//                                for (Player player : Bukkit.getOnlinePlayers()) {
-//                                    player.sendTitle(ChatColor.YELLOW + "1", "");
-//                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
-//                                }
-//                                break;
-//                            case 20:
-//                                for (Player player : Bukkit.getOnlinePlayers()) {
-//                                    Location boatRaceStartLocation = new Location(MazeTour.getWorld("world"), -3000, 100, 0);
-//                                    Boat boat = (Boat) player.getWorld().spawnEntity(player.getLocation(), EntityType.BOAT);
-//                                    boat.addPassenger(player);
-//                                    player.sendTitle(ChatColor.RED + "Старт", "");
-//
-//                                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
-//                                    player.playSound(player.getLocation(), Sound.ENTITY_WITCH_CELEBRATE, 1, 1);
-//                                    run = true;
-//                                }
-//                                break;
-//                        }
-//                        time++;
-//                    }
-//                }, 0, 20L).getTaskId();
-//        }
-//        World boatRaceWorld = Bukkit.getServer().getWorld("boat_race");
-//        Location boatRaceStartLocation = new Location(boatRaceWorld, 0, 100, 0);
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Выбери действие!");
+            return false;
+        }
+        switch (args[0]) {
+            case "start":
+                if (run) {
+                    sender.sendMessage(ChatColor.RED + "Испытание уже начато!");
+                    break;
+                }
+                run = true;
+                World world = (World) Bukkit.getServer().getWorld("world");
+                Bukkit.broadcastMessage(ChatColor.YELLOW + "Другое испытание начнется через 30 секунд!");
+                taskId = Bukkit.getScheduler().runTaskTimer(plugin, new Runnable() {
+                    int time = 0;
+
+                    @Override
+                    public void run() {
+                        switch (time) {
+                                                //case 20:
+                                                  //  Bukkit.broadcastMessage(ChatColor.YELLOW + "Перемещение на другое испытание через 10 секунд!");
+                                                    //break;
+                            case 0:
+                                World world = (World) Bukkit.getServer().getWorld("world");
+                                Location location = new Location(world, 1500, 63, 0);
+                                for (Player player : Bukkit.getOnlinePlayers()) {
+                                    player.teleport(location);
+                                }
+                                break;
+                            case 17:
+                                for (Player player : Bukkit.getOnlinePlayers()) {
+                                    player.sendTitle(ChatColor.YELLOW + "3", "");
+                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
+                                }
+                                break;
+                            case 18:
+                                for (Player player : Bukkit.getOnlinePlayers()) {
+                                    player.sendTitle(ChatColor.YELLOW + "2", "");
+                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
+                                }
+                                break;
+                            case 19:
+                                for (Player player : Bukkit.getOnlinePlayers()) {
+                                    player.sendTitle(ChatColor.YELLOW + "1", "");
+                                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2);
+                                }
+                                break;
+                            case 20:
+                                for (Player player : Bukkit.getOnlinePlayers()) {
+                                    Location boatRaceStartLocation = new Location(MazeTour.getWorld("world"), -3000, 100, 0);
+                                    Boat boat = (Boat) player.getWorld().spawnEntity(player.getLocation(), EntityType.BOAT);
+                                    boat.addPassenger(player);
+                                    player.sendTitle(ChatColor.RED + "Старт", "");
+
+                                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+                                    player.playSound(player.getLocation(), Sound.ENTITY_WITCH_CELEBRATE, 1, 1);
+                                    run = true;
+                                }
+                                break;
+                        }
+                        time++;
+                    }
+                }, 0, 20L).getTaskId();
+        }
+       World boatRaceWorld = Bukkit.getServer().getWorld("boat_race");
+        Location boatRaceStartLocation = new Location(boatRaceWorld, 0, 100, 0);
                 return false;
 
     }
